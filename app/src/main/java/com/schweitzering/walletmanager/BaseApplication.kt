a6acfa9cd30834e819df89d6973244c5804d0381
Kevin <kevschweitzer@gmail.com>
2019-09-23 23:35:20 -0300

    Adding DI Koin

---
+++ app/src/main/java/com/schweitzering/walletmanager/BaseApplication.kt
@@ -0,0 +1,20 @@
+package com.schweitzering.walletmanager
+
+import android.app.Application
+import com.schweitzering.data.inject.dataModule
+import com.schweitzering.walletmanager.inject.appModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class BaseApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            // Android context
+            androidContext(this@BaseApplication)
+            // modules
+            modules(listOf(appModule, dataModule))
+        }
+    }
+}

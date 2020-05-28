0509ce38f532e741bd0c5e6cb4f41647f87001ac
Kevin <kevschweitzer@gmail.com>
2020-05-28 02:50:10 -0300

    [TASK-26] Define Category create/edit flow

--- app/src/main/java/com/schweitzering/walletmanager/settings/categories/CategoriesSettingsViewModel.kt
+++ app/src/main/java/com/schweitzering/walletmanager/settings/categories/CategoriesSettingsViewModel.kt
@@ -1,11 +1,27 @@
 package com.schweitzering.walletmanager.settings.categories
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
+import com.schweitzering.domain.categories.CategoryType
 import com.schweitzering.domain.categories.GetAllCategoriesTypesUseCase
 
 class CategoriesSettingsViewModel(private val getAllCategoriesTypesUseCase: GetAllCategoriesTypesUseCase) {
 
+    sealed class State {
+        class CreateCategory(category: CategoryType): State()
+        class EditCategory(category: CategoryType): State()
+    }
+
     val categories = Transformations.map(getAllCategoriesTypesUseCase.execute()) {
         it.sortedBy { it.category.name }
+    }
+    val state = MutableLiveData<State>()
+
+    fun onCreateCategoryClicked(category: CategoryType) {
+        state.value = State.CreateCategory(category)
+    }
+
+    fun onEditCategoryClicked(category: CategoryType) {
+        state.value = State.EditCategory(category)
     }
 }
